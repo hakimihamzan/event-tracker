a77394049598840a340b89f4ba7a753ce4d0f50c
hakimihamzan <hakimihamzan20@gmail.com>
2021-01-24 12:22:10 +0800

    v2 project init

--- student/persona/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Participant
-
-class ParticipantSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Participant
-        fields = ('id', 'name', 'student_id', 'programme_code', 'faculty', 'campus', 'location', 'time', 'class_attended', 'lecturer', 'unique')
